970091d735bb784f104ef6a930ae779dada8123f
Mahima Bhutani <mahi.bhutani7@gmail.com>
2021-03-04 16:05:54 +0200

    Cleanup

--- src/helpers/openExternalLink.ts
+++
@@ -1,6 +0,0 @@
-export default function openExternalLink(
-  url: string,
-  target: string = "_blank"
-) {
-  window.open(url, target);
-}
--- src/helpers/typeDefinitions.ts
+++
@@ -1,9 +0,0 @@
-export interface Project {
-  order?: number;
-  id: string;
-  name: string;
-  image: string;
-  description?: string;
-  logo?: string;
-  accent?: string;
-}
